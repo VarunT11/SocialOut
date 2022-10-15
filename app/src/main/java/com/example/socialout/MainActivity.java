65ddc37cabbbdc43c8ba8480fec48643456c9574
starboi02 <varunbhardwaj.064@gmail.com>
2022-10-15 18:29:19 +0530

    story

--- app/src/main/java/com/example/socialout/MainActivity.java
+++ app/src/main/java/com/example/socialout/MainActivity.java
@@ -5,14 +5,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayoutManager HorizontalLayout;
+    FloatingActionButton filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        filter = findViewById(R.id.filter);
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FilterDialogFragment newFragment = new FilterDialogFragment();
+                newFragment.show(getSupportFragmentManager(),"Dialog");
+            }
+        });
 
         User[] myListData = new User[]{new User(),new User(),new User(),new User(),new User(),new User(),new User(),new User(),new User(),new User(),new User(),new User()};
 
